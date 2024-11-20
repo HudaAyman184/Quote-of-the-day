20c406a42392717b34469e0395cd41dccccbfb93
HudaAyman184 <hudaayman91@gmail.com>
2024-11-20 22:45:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,365 @@
+var Quotes = [
+    {
+        quote:"“Be yourself; everyone else is already taken.”",
+        by:"― Oscar Wilde",
+    },
+    {
+        quote:"“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+        by:"― Bernard M. Baruch",
+    },
+    {
+        quote:"“You only live once, but if you do it right, once is enough.”",
+        by:" ― Mae West",
+    },
+    {
+        quote:" “Be the change that you wish to see in the world.”",
+        by:"― Mahatma Gandhi",
+    },
+    {
+        quote:"“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
+        by:"― Mahatma Gandhi",
+    },
+    {
+        quote:"“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.”",
+        by:"― Albert Einstein",
+    },
+    {
+        quote:"“That which does not kill us makes us stronger.”",
+        by:"― Friedrich Nietzsche",
+    },
+    {
+        quote:"“It is never too late to be what you might have been.”",
+        by:"― George Eliot",
+    },
+    {
+        id: 9,
+        quote:"“There is no greater agony than bearing an untold story inside you.”",
+        by:"― Maya Angelou, I Know Why the Caged Bird Sings",
+    },
+    {
+        id: 10,
+        quote:"“We don't see things as they are, we see them as we are.”",
+        by:"― Anaïs Nin",
+    },
+    {
+        id: 11,
+        quote:"“Be yourself; everyone else is already taken.”",
+        by:"― Oscar Wilde",
+    },
+    {
+        id: 12,
+        quote:"“It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.”",
+        by:"― J.K. Rowling, Harry Potter and the Sorcerer's Stone",
+    },
+    {
+        id: 13,
+        quote:"“Life isn't about finding yourself. Life is about creating yourself.”",
+        by:"― George Bernard Shaw",
+    },
+    {
+        id: 14,
+        quote:"“If you want your children to be intelligent, read them fairy tales. If you want them to be more intelligent, read them more fairy tales.”",
+        by:"― Albert Einstein",
+    },
+    {
+        id: 15,
+        quote:"“The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.”",
+        by:"― Bob Marley",
+    },
+    {
+        id: 16,
+        quote:"“There is no friend as loyal as a book.”",
+        by:"― Ernest Hemingway",
+    },
+    {
+        id: 17,
+        quote:"“Success is not final, failure is not fatal: it is the courage to continue that counts.”",
+        by:"― Winston S. Churchill",
+    },
+    {
+        id: 18,
+        quote:"“Love is like the wind, you can't see it but you can feel it.”",
+        by:"― Nicholas Sparks, A Walk to Remember",
+    },
+    {
+        id: 19,
+        quote:"“Not all of us can do great things. But we can do small things with great love.”",
+        by:"― Mother Teresa",
+    },
+    {
+        id: 20,
+        quote:"“Life is like riding a bicycle. To keep your balance, you must keep moving.”",
+        by:"― Albert Einstein",
+    },
+    {
+        id: 21,
+        quote:"“Knowing yourself is the beginning of all wisdom.”",
+        by:"― Aristotle",
+    },
+    {
+        id: 22,
+        quote:"“Only the very weak-minded refuse to be influenced by literature and poetry.”",
+        by:"― Cassandra Clare, Clockwork Angel",
+    },
+    {
+        id: 23,
+        quote:"“Perhaps one did not want to be loved so much as to be understood.”",
+        by:"― George Orwell, 1984",
+    },
+    {
+        id: 24,
+        quote:"“Just when you think it can't get any worse, it can. And just when you think it can't get any better, it can.”",
+        by:"― Nicholas Sparks, At First Sight",
+    },
+    {
+        id: 25,
+        quote:"“Be nice to nerds. You may end up working for them. We all could.”",
+        by:"― Charles J. Sykes",
+    },
+    {
+        id: 26,
+        quote:"“يمكن أن تحكم الناس بالخوف والقمع ، لكن الخائفين لايمكن ان ينتصروا في حرب ، في ساحة الحرب يجب أن يكونوا أحراراً”",
+        by:"― بهاء طاهر, واحة الغروب",
+    },
+    {
+        id: 27,
+        quote:"“من العسير على نفسي أن أتصور الجمال غير مقترن بالفضيلة، الجمال الحق والفضيلة الحقة شيئ واحد”",  
+        by:"― توفيق الحكيم",
+    },
+    {
+        id: 28,
+        quote:"“أنا لست مجنونا حتى أقرأ الأوراق من أولها كما يقرأ العقلاء”",
+        by:"― توفيق الحكيم, يوميات نائب في الأرياف",        
+    },
+    {
+        id: 29,
+        quote:"“نادراً ماكنا نخرج من البيت ، لم يكن أحدنا يحتاج غير الآخر .. حتى طفلنا لم يكن ثالثنا في البيت بل كنا كلانا فيه معاُ ، لم يكن في دنيانا غيرها وغيري”",
+        by:"― بهاء طاهر, نقطة النور",        
+    },
+    {
+        id: 30,
+        quote:"“و أعجبُ كيف يُغريني طريقي، وموتِي فيهِ أقربُ من نجاحي.”",
+        by:"― غازي القصيبي",        
+    },
+    {
+        id: 31,
+        quote:"“كان التاريخ في ذلك الوقت، كما كان في أكثر الأوقات ،أرستقراطياً لا يحفل إلا بالسادة ،ولا يلتفت إلا إلى القادة”",
+        by:"― طه حسين, الوعد الحق",     
+    },
+    {
+        id: 32,
+        quote:"“كلما كانت الكذبة كبيرة جدا واسعة جدا صارت أقرب للتصديق, لأنه - ببساطة - لن يتصور أحد أن هناك كذبة بهذا الحجم”",
+        by:"― أحمد خالد توفيق, شآبيب",        
+    },
+    {
+        id: 33,
+        quote:"“لكَ أنْ تتخيَّل، لو أنَّ كُلّ ظلامِ الدُّنيا اجْتمع على شمعةٍ واحدةٍ، فلن يقدر على أنْ يُطفئها، ولعل الشّمعة الّتي تُبقينا على قيد الأمل، هي تلك الآية: أَلَيْسَ اللَّهُ بِكَافٍ عَبْدَهُ.”",
+        by:"― حسين علي السنافي",        
+    },
+    {
+        id: 34,
+        quote:"“الإنسانية هي فن صنع الحب دون تكوين عدو”",
+        by:"― P.S. Jagadeesh Kumar",        
+    },
+    {
+        id: 35,
+        quote:"“وحقي فيك يا وطني غريق”",
+        by:"― Khaled Ibrahim",        
+    },
+    {
+        id: 36,
+        quote:"“ما الحب إلا سقام في القلوب، ياتي بعشق وبعدها تنهار الهموم”",
+        by:"― Raouf Ayoub",        
+    },
+    {
+        id: 37,
+        quote:"“وأَمَرْتُ قلبي بالتريّث : كُنْ حياديّاً كأنَّكَ لَسْتَ مني”",
+        by:"― محمود درويش",        
+    },
+    {
+        id: 38,
+        quote:"“أَيَمْرضُ حُلْمٌ كَمَا يَمْرَضُ الحَالِمُون؟ خَريفٌ خريفٌ. أيُولَدُ شَعْبٌ عَلَى مِقْصلَهْ؛يحِقُّ لَنَا أنْ نَمُوتَ كمَا نَشْتَهِي أنْ نَمْوت، لِتَخْتَبِىء الأرضُ في سُنْبُلَهْ”",      
+        by:"― محمود درويش, ورد أقل",        
+    },
+    {
+        id: 39,
+        quote:"“جرب صدق حسن الظن في تعاملك مع الناس , وستشعر بإرتياح نفسي ولن تأخذ الأمور بشكل شخصي أو بعصبية”",
+        by:"― احمد الشقيري ",     
+    },
+    {
+        id: 40,
+        quote:"“إن مشاعر التعصب لجنس من الاجناس ماتت في دمي لأني مسلم، غير أن التحمُّس للعروبة وأدبها غلبني في هذه الأونة، إذ أحسستُ كأن التضحية بالعرب ولغتهم بعض ما تكنُّه السياسة الدولية في ضميرها الملوَّث وبعض ما تسخر له أتباعها وأذنابها”",
+        by:"― محمد الغزالي, جدد حياتك",        
+    },
+    {
+        id: 41,
+        quote:"“Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.”",
+        by:"― Lao Tzu",        
+    },
+    {
+        id: 42,
+        quote:"“Never put off till tomorrow what may be done day after tomorrow just as well.”",
+        by:"― Mark Twain",        
+    },
+    {
+        id: 43,
+        quote:"“Life isn't about finding yourself. Life is about creating yourself.”",
+        by:"― George Bernard Shaw",        
+    },
+    {
+        id: 44,
+        quote:"“Today you are You, that is truer than true. There is no one alive who is Youer than You.”",
+        by:"― Dr. Seuss, Happy Birthday to You!",        
+    },
+    {
+        id: 45,
+        quote:"“I'm not afraid of death; I just don't want to be there when it happens.”",
+        by:"― Woody Allen",        
+    },
+    {
+        id: 46,
+        quote:"“Logic will get you from A to Z; imagination will get you everywhere.”",
+        by:"― Albert Einstein",        
+    },
+    {
+        id: 47,
+        quote:"“The more that you read, the more things you will know. The more that you learn, the more places you'll go.”",
+        by:"― Dr. Seuss, I Can Read With My Eyes Shut!",        
+    },
+    {
+        id: 48,
+        quote:"“When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.”",
+        by:"― Helen Keller",        
+    },
+    {
+        id: 49,
+        quote:"“Some people never go crazy. What truly horrible lives they must lead.”",
+        by:"― Charles Bukowski",        
+    },
+    {
+        id: 50,
+        quote:"“I declare after all there is no enjoyment like reading! How much sooner one tires of any thing than of a book! -- When I have a house of my own, I shall be miserable if I have not an excellent library.”",
+        by:"― Jane Austen, Pride and Prejudice",        
+    },
+    {
+        id: 51,
+        quote:"“The reason I talk to myself is because I’m the only one whose answers I accept.”",
+        by:"― George Carlin",
+    },
+    {
+        id: 52,
+        quote:"“There is nothing to writing. All you do is sit down at a typewriter and bleed.”",
+        by:"― Ernest Hemingway",
+    },
+    {
+        id: 53,
+        quote:"“You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.”",
+        by:"― John Lennon",
+    },
+    {
+        id: 54,
+        quote:"“Books are a uniquely portable magic.”",
+        by:"― Stephen King, On Writing: A Memoir of the Craft",
+    },
+    {
+        id: 55,
+        quote:"“Anyone who has never made a mistake has never tried anything new.”",
+        by:"― Albert Einstein",
+    },
+    {
+        id: 56,
+        quote:"“It’s no use going back to yesterday, because I was a different person then.”",
+        by:"― Lewis Carroll",
+    },
+    {
+        id: 57,
+        quote:"“The truth is rarely pure and never simple.”",
+        by:"― Oscar Wilde, The Importance of Being Earnest",
+    },
+    {
+        id: 58,
+        quote:"“What a slut time is. She screws everybody.”",
+        by:"― John Green, The Fault in Our Stars",
+    },
+    {
+        id: 59,
+        quote:"“It takes courage to grow up and become who you really are.”",
+        by:"― E.E. Cummings",
+    },
+    {
+        id: 60,
+        quote:"But better to get hurt by the truth than comforted with a lie.”",
+        by:"― Khaled Hosseini",
+    },
+    {
+        id: 61,
+        quote:"“... a mind needs books as a sword needs a whetstone, if it is to keep its edge.”",
+        by:"― George R.R. Martin, A Game of Thrones",
+    },
+    {
+        id: 62,
+        quote:"“We believe in ordinary acts of bravery, in the courage that drives one person to stand up for another.”",
+        by:"― Veronica Roth, Divergent",
+    },
+    {
+        id: 63,
+        quote:"“Memories warm you up from the inside. But they also tear you apart.”",
+        by:"― Haruki Murakami, Kafka on the Shore",
+    },
+    {
+        id: 64,
+        quote:"“A lie can travel half way around the world while the truth is putting on its shoes.”",
+        by:"― Mark Twain",
+    },
+    {
+        id: 65,
+        quote:"“Never tell the truth to people who are not worthy of it.”",
+        by:"― Mark Twain",
+    },
+    {
+        id: 66,
+        quote:"“Knowing yourself is the beginning of all wisdom.”",
+        by:"― Aristotle",
+    },
+    {
+        id: 67,
+        quote:"“It's the possibility of having a dream come true that makes life interesting.”",
+        by:"― Paulo Coelho, The Alchemist",
+    },
+    {
+        id: 68,
+        quote:"“Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them.”",
+        by:"― William Shakespeare, Twelfth Night",
+    },
+    {
+        id: 69,
+        quote:"“Happiness in intelligent people is the rarest thing I know.”",
+        by:"― Ernest Hemingway, The Garden of Eden",
+    },
+    {
+        quote:"“Numbing the pain for a while will make it worse when you finally feel it.”",
+        by:"― J.K. Rowling, Harry Potter and the Goblet of Fire",
+    },
+]
+// var lastindex=-1;
+// function getQuote(){
+//     let randomindex
+//     for(randomindex=Math.floor((Math.random() *Quotes.length ) );randomindex===lastindex;randomindex=Math.floor((Math.random() *Quotes.length ) ))
+//     {
+//         lastindex=randomindex;
+//     }
+//     document.getElementById("quote").innerHTML=Quotes[randomindex].quote;
+//     document.getElementById("by").innerHTML=Quotes[randomindex].by;
+// }
+
+
+
+const quoteText = document.getElementById("quote");
+const quoteby = document.getElementById("by");
+const newQuoteButton = document.getElementById("new-quote");
+function getNewQuote() {
+    const randomIndex = Math.floor(Math.random() *Quotes.length);
+    quoteText.textContent = Quotes[randomIndex].quote
+    quoteby.textContent = Quotes[randomIndex].by
+}
+newQuoteButton.addEventListener("click", getNewQuote);
